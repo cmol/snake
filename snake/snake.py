63aaac06fb9a8370cf8c90726ac55c14817336ae
Claus Lensbøl <cmol@cmol.dk>
2012-07-31 14:08:33 +0200

    Renamed tail add

--- snake/snake.py
+++ snake/snake.py
@@ -3,7 +3,7 @@ class Snake(object):
   
   # Variables for Snake objects
   snake_arr = [[3,3], [2,3], [1,3]]
-  add = False
+  add_tail = False
   direction = 0
 
   # Method for moving the snake
@@ -17,13 +17,13 @@ class Snake(object):
     elif self.direction == 3:
       self.snake_arr.insert(0, [self.snake_arr[0][0], self.snake_arr[0][1]-1])
       
-    if self.add:
-      self.add = False
+    if self.add_tail:
+      self.add_tail = False
     else:
       self.snake_arr.pop()
       
   def add(self):
-    self.add = True
+    self.add_tail = True
   
   def position(self):
     return self.snake_arr
