1f57cc39db4cec9c602ef7a2b17f2e99c3c033b7
Claus Lensbøl <cmol@cmol.dk>
2012-07-31 13:07:59 +0200

    Moved snake logic to Snake class

--- snake/game.py
+++ snake/game.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys, pygame
+from snake import Snake
 
 pygame.init()
 
@@ -8,17 +9,19 @@ bg_color = 0, 0, 0
 done = False
 
 grid_size = 100
-block_size = 5
+BLOCK_SIZE = 5
 
 pos_x, pos_y = 0, 0
 direction = 0
 
-snake_head_color = 18, 255, 00
+SNAKE_HEAD = 18, 255, 00
 
 clock=pygame.time.Clock()
 
+snake1 = Snake()
+
 # Inititalize screen and set caption
-size = width, height = grid_size*block_size, grid_size*block_size
+size = width, height = grid_size*BLOCK_SIZE, grid_size*BLOCK_SIZE
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Snake")
 
@@ -33,34 +36,30 @@ while done==False:
   if event.type == pygame.KEYDOWN:
     # Figure out if it was an arrow key, and set direction
     if event.key == pygame.K_LEFT:
-      direction = 2
+      snake1.direc(2)
     if event.key == pygame.K_RIGHT:
-      direction = 0
+      snake1.direc(0)
     if event.key == pygame.K_UP:
-      direction = 3
+      snake1.direc(3)
     if event.key == pygame.K_DOWN:
-      direction = 1
-  
-  # Move the snake
-  if direction == 0:
-    pos_x +=1
-  if direction == 1:
-    pos_y +=1
-  if direction == 2:
-    pos_x -=1
-  if direction == 3:
-    pos_y -=1
-  
+      snake1.direc(1)
+      
   if pos_x >= grid_size or pos_y >= grid_size or pos_x < 0 or pos_y < 0:
     sys.exit()
   
   # Limit to 60 FPS
+  snake1.move()
   clock.tick(60)
-  print (clock.get_fps())
+#  print (clock.get_fps())
   
-  # Draw stuff
+  # Clear screen and draw background
   screen.fill(bg_color)
-  draw_square(screen, [pos_x*block_size,pos_y*block_size,block_size,block_size], snake_head_color)
+  
+  # Draw the snake itself
+  for point in snake1.position():
+      draw_square(screen, [point[0]*BLOCK_SIZE,point[1]*BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE], SNAKE_HEAD)
+  
+  # Flip the buffer to the display to show the snake
   pygame.display.flip()
   
 pygame.quit()
