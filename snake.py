6efc7338fcc7f49fd8fa6a1a5735d6a398d092a7
heigren <heigren@con-rw.(none)>
2012-08-02 14:11:21 +0200

    Initial multiplayer-code

--- snake.py
+++ snake.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 import argparse
 import snake
+from twisted.internet import threads, protocol, reactor
+
+class SnakeRemote(protocol.Protocol):
+  def connectionMade(self):
+    print("Con made")
+    #raise Exception("!")
+    self.snakie = snake.game.Game.spawnSnake()
+        
+  def dataReceived(self, data):
+    data = int(data.rstrip())
+    snake.game.Game.event_map[data][0].direc(snake.game.Game.event_map[data][1])
+
+class SnakeFactory(protocol.Factory):
+  def buildProtocol(self, addr):
+    return SnakeRemote()
 
 parser = argparse.ArgumentParser(prog='snake.py',
                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -12,4 +27,9 @@ parser.add_argument('-b', '--block', type=int, default=5, help='Block size')
 args = parser.parse_args()
 
 game = snake.game.Game(args.players, args.grid, args.block, args.fullscreen)
-game.start_game()
+reactor.listenTCP(9999, SnakeFactory())
+#reactor.callInThread(game.start_game())
+
+#game.start_game()
+game.start()
+reactor.run()
