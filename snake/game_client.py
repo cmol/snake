60d92483d0be072b4225371874bbe7c9b93a1aca
Claus Lensbøl <cmol@cmol.dk>
2012-08-07 13:29:28 +0200

    Added game client for connecting to the server (default is local server)

--- snake/game_client.py
+++ snake/game_client.py
@@ -1,18 +1,119 @@
 import cPickle
-from threading import Thread
 from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
-from snake import Snake  
+import pygame
+from snake import Snake
+from cheese import Cheese
+
+pygame.init()
   
 class GameClient(LineReceiver):
+  # End string
   end = "DEAD"
+  direction = None
   
+  # Colors
+  bg_color = 0, 0, 0
+  CHEESE_COL = 255, 255, 0
+  
+  # Vars for screen building (TODO: Move this data to server)
+  block_size = 5
+  grid_size = 100
+  grid_x = grid_size
+  grid_y = grid_size
+  
+  # Directions
+  DIRECTION_UP = 1 
+  DIRECTION_DOWN = 2 
+  DIRECTION_LEFT = 3 
+  DIRECTION_RIGHT = 4
+  
+  # Event keys
+  event_map = {pygame.K_LEFT: DIRECTION_LEFT,
+      pygame.K_UP: DIRECTION_UP,
+      pygame.K_RIGHT: DIRECTION_RIGHT,
+      pygame.K_DOWN: DIRECTION_DOWN}
+  
+  def __init__(self):
+    # Initialize screen 
+    size = width, height = self.grid_x*self.block_size, self.grid_y*self.block_size
+    self.screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Snake")
+    
+  # Draw a square
+  def draw_square(self, screen, rect, color):
+    screen.fill(color, rect)
+  
+  # Check for events (key press etc.)  
+  def check_events(self):
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT: self.end_game()
+      # Check for key presses and if they are in our event_map
+      if event.type == pygame.KEYDOWN:
+        if event.key in self.event_map:
+          self.sendLine(str(self.event_map[event.key]))
+        elif event.key == pygame.K_ESCAPE:
+         self.end_game()
+  
+  def draw_screen(self):
+    # Paint everything black
+    self.screen.fill(self.bg_color)
+    
+    # Draw the cheeses
+    for cheese in self.cheeses:
+      self.draw_square(
+        self.screen,
+          [
+            self.cheese.position()[0]*self.block_size,
+            self.cheese.position()[1]*self.block_size,
+            self.block_size*self.cheese.size,
+            self.block_size*self.cheese.size
+          ],
+          self.CHEESE_COL
+        )
+      
+    # Draw the snake itself
+    for snake in self.snakes:
+      for point in snake.position():
+        self.draw_square(
+          self.screen,
+          [
+            point[0]*self.block_size,
+            point[1]*self.block_size,
+            self.block_size,
+            self.block_size
+          ],
+          snake.color()
+        )
+    
+    # Show everything
+    pygame.display.flip()
+    
+        
+  def play_round(self):
+    self.draw_screen()
+    self.check_events()
+        
+  # End the game nicely
+  def end_game(self):
+    exit() # That's not nice!
+  
+  # ==== From here networking occurs ====  
   def connectionMade(self):
     print("Connection made")
+    
+  def dataReceived(self, p_data):
+    data = cPickle.loads(p_data)
+    self.snakes = data["snakes"]
+    self.cheeses = data["cheeses"]
+    self.play_round()
 
   def lineReceived(self, line):
-    print "receive:", line
+    data = dumps(line)
+    print data
+    self.snakes = data["Snakes"]
+    self.play_rounds()
     if line==self.end:
       self.transport.loseConnection()
 
